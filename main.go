60f266525278606e9a464fa53a366dc1e980758f
侯锐 <hourui@qianmi.com>
2019-01-02 15:17:01 +0800

    简陋的完成最初版本

---
+++ main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"github.com/ritterhou/stinger/network"
+	"log"
+	"net"
+	"os"
+	"strconv"
+)
+
+func init() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetOutput(os.Stdout)
+}
+
+const localPort = 2680
+
+func main() {
+	var l net.Listener
+	var err error
+	var host = "0.0.0.0:" + strconv.Itoa(localPort)
+
+	l, err = net.Listen("tcp", host)
+	if err != nil {
+		log.Fatal("Error listening:", err)
+	}
+	defer l.Close()
+
+	log.Println("Listening on " + host + " ...")
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Println("Error accepting:", err)
+			continue
+		}
+
+		//log.Printf("Connection established %s -> %s \n", conn.RemoteAddr(), conn.LocalAddr())
+		c := network.Connection{Conn: conn}
+		go handlerSocks5(c)
+	}
+}
+
+func handlerSocks5(conn network.Connection) {
+	//defer conn.Close()
+
+	authSocks5(conn)
+	remoteConn := connectSocks5(conn)
+	handlerSocks5Data(conn, remoteConn)
+
+}
+
+func authSocks5(conn network.Connection) {
+	socksVersion := conn.Read(1)[0]
+	if socksVersion != 5 {
+		log.Fatal("Socks version should be 5, now is", socksVersion)
+	}
+
+	authWaysNum := conn.Read(1)[0]
+	authWays := conn.Read(uint32(authWaysNum))
+	if !in(byte(0), authWays) {
+		log.Fatal("Only support [NO AUTHENTICATION REQUIRED] auth way.")
+	}
+
+	conn.Write([]byte{5, 0})
+
+	//log.Println("Auth success ", conn.RemoteAddress())
+}
+
+func connectSocks5(conn network.Connection) network.Connection {
+	socksVersion := conn.Read(1)[0]
+	if socksVersion != 5 {
+		log.Fatal("Socks version should be 5, now is", socksVersion)
+	}
+
+	command := conn.Read(1)[0]
+	if command != 1 {
+		log.Fatal("Only support [CONNECT] command")
+	}
+
+	conn.Read(1) // 保留字
+
+	addrType := conn.Read(1)[0]
+
+	var host string
+	switch addrType {
+	case 1: // ipv4
+		data := conn.Read(4)
+		host = fmt.Sprintf("%d.%d.%d.%d", data[0], data[1], data[2], data[3])
+	case 3: // 域名
+		hostLength := conn.Read(1)[0]
+		host = string(conn.Read(uint32(hostLength)))
+	default:
+		log.Fatal("Not support address type", addrType)
+	}
+
+	port := binary.BigEndian.Uint16(conn.Read(2))
+
+	addr := host + ":" + strconv.Itoa(int(port))
+
+	c, err := net.Dial("tcp", addr)
+	if err != nil {
+		log.Fatal("Can't connect to", addr)
+	}
+
+	conn.Write([]byte{5, 0, 0, 1, 0, 0, 0, 0, 0, 0})
+
+	remoteConn := network.Connection{Conn: c}
+
+	log.Printf("Connect success %s -> %s, %s => %s\n",
+		conn.RemoteAddress(), conn.LocalAddress(),
+		remoteConn.LocalAddress(), remoteConn.RemoteAddress())
+
+	return remoteConn
+}
+
+func handlerSocks5Data(localConn network.Connection, remoteConn network.Connection) {
+	go func() {
+		for {
+			buf := localConn.Read(1024)
+			//log.Println(string(buf))
+			remoteConn.Write(buf)
+		}
+	}()
+
+	go func() {
+		for {
+			buf := remoteConn.Read(1024)
+			//log.Println(string(buf))
+			localConn.Write(buf)
+		}
+	}()
+}
+
+func in(num byte, list []byte) bool {
+	for _, e := range list {
+		if e == num {
+			return true
+		}
+	}
+	return false
+}
---
+++ network/connection.go
@@ -0,0 +1,45 @@
+package network
+
+import (
+	"log"
+	"net"
+)
+
+type Connection struct {
+	Conn net.Conn
+}
+
+func (c Connection) Read(length uint32) []byte {
+	conn := c.Conn
+
+	var buf = make([]byte, length)
+	var bufSize, err = conn.Read(buf)
+	//if bufSize == 0 {
+	//	log.Printf("Connection closed by client %s", conn.RemoteAddr())
+	//	return nil
+	//}
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	return buf[:bufSize]
+}
+
+func (c Connection) Write(data []byte) {
+	_, err := c.Conn.Write(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (c Connection) Close() {
+	c.Conn.Close()
+}
+
+func (c Connection) RemoteAddress() string {
+	return c.Conn.RemoteAddr().String()
+}
+
+func (c Connection) LocalAddress() string {
+	return c.Conn.LocalAddr().String()
+}
