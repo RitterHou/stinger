4e961b45f743fe9e60e599c55479951ff576c3bc
侯锐 <hourui@qianmi.com>
2019-01-03 18:07:19 +0800

    优化流量单位的计算方法

--- core/common/util.go
+++ core/common/util.go
@@ -4,20 +4,13 @@ import "fmt"
 
 func ByteFormat(length uint64) string {
 	size := float64(length)
-	if size < 1024 {
-		return fmt.Sprintf("%7.2fB", size)
+	units := []string{"B", "K", "M", "G", "T"}
+
+	for _, unit := range units {
+		if size < 1024 {
+			return fmt.Sprintf("%7.2f%s", size, unit)
+		}
+		size = size / 1024.0
 	}
-	size = size / 1024.0
-	if size < 1024 {
-		return fmt.Sprintf("%7.2fK", size)
-	}
-	size = size / 1024.0
-	if size < 1024 {
-		return fmt.Sprintf("%7.2fM", size)
-	}
-	size = size / 1024.0
-	if size < 1024 {
-		return fmt.Sprintf("%7.2fG", size)
-	}
-	return "Too larger size."
+	return "Size larger than 1024TB."
 }
